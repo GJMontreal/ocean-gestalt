c2108d7ce081e2ebb1e5a85bd4045ab35490896c
Geoffrey Jones <bashmntl@gmail.com>
2025-01-10 11:13:01 -0500

    More pruning

--- src/Ocean.hpp
+++ src/Ocean.hpp
@@ -1,7 +1,6 @@
 #ifndef OCEAN_HPP
 #define OCEAN_HPP
 
-// #include <IOKit/hid/IOHIDUsageTables.h> // where did this come from
 #include "Model.hpp"
 #include "Configuration.hpp"
 #include "Wave.hpp"
@@ -9,11 +8,10 @@
 
 #include <glm/glm.hpp>
 
-using vec3 = glm::vec3;
 
 class Ocean : public Model {
  public:
-  Ocean(std::shared_ptr<Configuration> configuration);
+  explicit Ocean(std::shared_ptr<Configuration> configuration);
 
   ~Ocean() override = default;
   void draw(Uniforms& uniforms) override;
@@ -23,15 +21,10 @@ class Ocean : public Model {
 
   void updateShaderUniforms() override;
  private:
-  // We'll only use this stuff if we're using the cpu to calculate our vertices
   std::shared_ptr<Configuration> configuration;
   std::vector<VertexType> particles;
   float elapsedTime=0;
   double lastTime;
-  void initParticles();
-  void resetParticles();
-  void moveParticles(float time);
-  vec3 gerstnerWave(float time, vec2 position, const Wave* wave) const;
 
   std::vector<Wave*> waves; //this is moving to the configuration
 
