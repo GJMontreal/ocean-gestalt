f31af59320d5556c86f0c5fe045da1a4856db36d
Geoffrey Jones <bashmntl@gmail.com>
2025-01-13 19:22:24 -0500

    Remove dead code

--- src/main.cpp
+++ src/main.cpp
@@ -13,8 +13,6 @@
 #include <memory>
 int main(int argc, const char* argv[]) {
   auto app = OceanGestalt();
-  // glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
-  glfwSetInputMode(app.getWindow(), GLFW_CURSOR, GLFW_CURSOR_DISABLED);
   app.run();
   return 0;
 }
