6692f04f588d4ba2090254d426d97bbf597b71e5
Geoffrey Jones <bashmntl@gmail.com>
2025-06-04 16:23:23 -0400

    our first endpoint

--- src/rest/PauseHandler.cpp
+++ src/rest/PauseHandler.cpp
@@ -1,23 +1,43 @@
 #include "PauseHandler.hpp"
 
 #include "PauseHandler.hpp"
+#include <nlohmann/json.hpp>
 #include <string>
-#include <sstream>
+
+using json = nlohmann::json;
 
 bool PauseHandler::handlePost(CivetServer*, struct mg_connection* conn) {
     char buffer[1024];
     int len = mg_read(conn, buffer, sizeof(buffer) - 1);
     buffer[len] = '\0';
 
-    std::string body(buffer);
-    //we should use a more reliable json parser here
-    bool pause = body.find("pause=true") != std::string::npos;
+    try {
+        json request = json::parse(buffer);
 
-    api.pauseSimulation(pause);
+        if (!request.contains("pause") || !request["pause"].is_boolean()) {
+            mg_printf(conn,
+                "HTTP/1.1 400 Bad Request\r\nContent-Type: text/plain\r\n\r\nMissing or invalid 'pause' field\n");
+            return true;
+        }
 
-    mg_printf(conn,
-        "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\n\r\nSimulation paused = %s\n",
-        pause ? "true" : "false"
-    );
-    return true;
+        bool pause = request["pause"];
+        api.pauseSimulation(pause);
+
+        json response = {
+            {"status", "ok"},
+            {"paused", pause}
+        };
+
+        std::string body = response.dump();
+
+        mg_printf(conn,
+            "HTTP/1.1 200 OK\r\nContent-Type: application/json\r\nContent-Length: %lu\r\n\r\n%s",
+            body.size(), body.c_str());
+        return true;
+
+    } catch (const json::parse_error& e) {
+        mg_printf(conn,
+            "HTTP/1.1 400 Bad Request\r\nContent-Type: text/plain\r\n\r\nInvalid JSON\n");
+        return true;
+    }
 }
